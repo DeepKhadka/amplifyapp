849a26c663d26236ab931cf08cf5cea3538567f4
DeepKhadka <dipesh.khadka9999@gmail.com>
2020-12-14 23:04:08 -0600

    Domain integration

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-   <div className="container">
-     <div className="firstDiv">
-
-     </div>
-     <div className="secondDiv">
-
-     </div>
-     
-   </div>
-  );
+import React, { Component } from "react";
+import "./App.css";
+export default class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Hi! My name is Deep.</h1>
+        <h2>This is going to be Portfolio.</h2>
+        <h3>Hang tight!</h3>
+      </div>
+    );
+  }
 }
-
-export default App;
